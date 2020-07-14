7fae063c8c106a56051debebaba4154aa77f9671
Hyukin Kown <901po3@gmail.com>
2020-07-14 11:29:38 +0900

    map1 added

---
+++ Assets/Scripts/CameraSize.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Class: CameraSize
+ * Date: 2020.7.14
+ * Author: Hyukin Kwon 
+ * Description: Adjusting Camera Size repect to screen size.
+*/
+
+public class CameraSize : MonoBehaviour
+{
+    [SerializeField] float size = 15;
+    void Start()
+    {
+        Camera.main.orthographicSize = (float)(size * Screen.height / Screen.width * 0.5);
+    }
+}
