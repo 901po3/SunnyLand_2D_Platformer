d4ebe79cda4881573f4dc5637bfafecb16046ab2
Hyukin Kown <901po3@gmail.com>
2020-07-16 01:24:18 +0900

    mat fixed

--- Assets/Scripts/MonsterPlant.cs
+++ Assets/Scripts/MonsterPlant.cs
@@ -33,19 +33,47 @@ public class MonsterPlant : MonoBehaviour
 
     private void Attack()
     {
-        isAttacking = false;
-        Collider2D Collider2D = Physics2D.OverlapCircle(faceCheck.position, checkRadius, whatIsSide);
-
-        if (Collider2D != null)
+        RaycastHit2D hit;
+        Vector2 dir = (faceCheck.transform.position - transform.position).normalized;
+        hit = Physics2D.Raycast(transform.position, dir, 1.5f);
+        if(hit.collider != null)
         {
-            isAttacking = true;
-            GetComponent<Animator>().SetTrigger("isAttacking");
-            PlayerController.instance.SetAttackingPlant(gameObject);
+            if (hit.transform.tag == "Player")
+            {
+                Debug.DrawRay(transform.position, dir * hit.distance, Color.yellow);
+                Debug.Log("Did Hit");
+                isAttacking = true;
+                GetComponent<Animator>().SetTrigger("isAttacking");
+                PlayerController.instance.SetAttackingPlant(gameObject);
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, dir * 1.5f, Color.white);
+                Debug.Log("Did not Hit");
+                PlayerController.instance.SetAttackingPlant(null);
+            }
         }
         else
         {
+            Debug.DrawRay(transform.position, dir * 1.5f, Color.white);
+            Debug.Log("Did not Hit");
             PlayerController.instance.SetAttackingPlant(null);
         }
+
+
+        //isAttacking = false;
+        //Collider2D Collider2D = Physics2D.OverlapCircle(faceCheck.position, checkRadius, whatIsSide);
+
+        //if (Collider2D != null)
+        //{
+        //    isAttacking = true;
+        //    GetComponent<Animator>().SetTrigger("isAttacking");
+        //    PlayerController.instance.SetAttackingPlant(gameObject);
+        //}
+        //else
+        //{
+        //    PlayerController.instance.SetAttackingPlant(null);
+        //}
     }
 
     protected void Flip()
